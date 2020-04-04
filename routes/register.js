e7b0a8f063e1b767f4892934a79995a745bb9262
ayz-lex <andrewyzhanglex@gmail.com>
2020-04-04 19:50:10 -0400

    fix upsert/create user

--- routes/register.js
+++ routes/register.js
@@ -3,23 +3,38 @@ const router = express.Router()
 const User = require('../databases/models/users')
 const Movie = require('../databases/models/movies')
 
+const createMovie = async (value) => {
+    const [movie, created] = await Movie.findOrCreate({
+        where: {movie: value},
+        defaults: {popularity: 0}
+    })
+    movie.popularity++
+}
+
+const createUser = async (body) => {
+    const [user, created] = await User.findOrCreate({
+        where: {username: body.username},
+        defaults: {
+            password: body.password,
+            occupation: body.occupation,
+            age: body.age
+        }
+    })
+    if (created) {
+        alert('Username Taken')
+    }
+}
+
 router.get('/', async (req, res) => {
     await res.render('register')
 })
 
 router.post('/', express.urlencoded({extended: true}), async (req, res) => {
     
-    const username = req.body.username
-    const password = req.body.password
-    const occupation = req.body.occupation
-    const age = req.body.age
-    console.log(username)
-    await User.create({username: username, password: password, occupation: occupation, age: age})
-
-    const movie1 = req.body.movie1
-    const movie2 = req.body.movie2
-    const movie3 = req.body.movie3
-    await Movie.create({movie: movie1})
+    createUser(req.body)
+    createMovie(req.body.movie1)
+    createMovie(req.body.movie2)
+    createMovie(req.body.movie3)
     res.redirect('/')
 })
 
