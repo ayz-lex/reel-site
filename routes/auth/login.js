5907b969c97f5e03b040205cb0e58bcf9ac7a834
ayz-lex <andrewyzhanglex@gmail.com>
2020-05-29 11:53:38 -0400

    fix async await stuff

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -4,25 +4,19 @@ const User = require('../../databases/models/users')
 const bcrypt = require('bcrypt')
 
 router.post('/', express.json(), async (req, res) => {
-  await User.findOne({where: {username: req.body.username}}).then((user) => {
-    if (user !== null) {
-      //username does exist
-      bcrypt.compare(req.body.password, user.password).then((result) => {
-        if (result) {
-          //right password
-          req.session.username = user.username
-          req.session.data = {occupation: user.occupation, age: user.age, watched: user.watched}
-          res.send('OK')
-        } else {
-          //wrong password
-          res.send('No Password')
-        }
-      })
+  let user = await User.findOne({where: {username: req.body.username}})
+  if (user !== null) {
+    let result = await bcrypt.compare(req.body.password, user.password)
+    if (result) {
+      req.session.username = user.username
+      req.session.data = {occupation: user.occupation, age: user.age, watched: user.watched}
+      res.send('OK')
     } else {
-      //username doesn't exist
-      res.send('NO Username')
+      res.send('No Password')
     }
-  })
+  } else {
+    res.send('No Username')
+  }
 })
 
 module.exports = router
--- routes/auth/register.js
+++ routes/auth/register.js
@@ -5,49 +5,41 @@ const Movie = require('../../databases/models/movies')
 const bcrypt = require('bcrypt')
 
 const upsertMovie = async (value) => {
-  await Movie.findOne({where: {movie: value}}).then(async (element) => {
-    if (element === null) {
-      await Movie.create({movie: value, popularity: 1})
-    } else {
-      element.popularity++
-      element.save()
-    }
-  })
+  let movie = await Movie.findOne({where: {movie: value}})
+  if (movie === null) {
+    await Movie.create({movie: value, popularity: 1})
+  } else {
+    ++movie.popularity
+    element.save()
+  }
 }
 
 const createUser = async (req) => {
-  return await User.findOne({where: {username: req.body.username}}).then((element) => {
-    if (element === null) {
-      const {username, password, occupation, age, movie1, movie2, movie3} = req.body
-      bcrypt.hash(password, 12, async (err, hash) => {
-        if (err) throw err
-        await User.create({username: username, password: hash, occupation: occupation, age: age, watched: [movie1, movie2, movie3]})
-      })
-      req.session.username = username
-      req.session.data = {occupation: occupation, age: age, watched: [movie1, movie2, movie3]}
-      return false
-    } else {
-      return true
-    }
-  })
+  let user = await User.findOne({where: {username: req.body.username}})
+  if (user === null) {
+    const {username, password, occupation, age, movie1, movie2, movie3} = req.body
+    bcrypt.hash(password, 12, async (err, hash) => {
+      if (err) throw err
+      await User.create({username: username, password: hash, occupation: occupation, age: age, watched: [movie1, movie2, movie3]})
+    })
+    req.session.username = username
+    req.session.data = {occupation: occupation, age: age, watched: [movie1, movie2, movie3]}
+    return false
+  } else {
+    return true
+  }
 }
 
 router.post('/', express.json(), async (req, res) => {
-  await createUser(req)
-    .then(async (bool) => {
-      if (!bool) {
-        await upsertMovie(req.body.movie1)
-        await upsertMovie(req.body.movie2)
-        await upsertMovie(req.body.movie3)
-        res.send('OK')
-      } else {
-        res.send('User Taken')
-      }
-    })
-    .catch(err => {
-      console.error(err)
-      res.sendStatus(500)
-    })
+  let checkCreated = await createUser(req)
+  if (!checkCreated) {
+    await upsertMovie(req.body.movie1)
+    await upsertMovie(req.body.movie2)
+    await upsertMovie(req.body.movie3)
+    res.send('OK')
+  } else {
+    res.send('User Taken')
+  }
 })
 
 module.exports = router
