c551492a849e26e376926afd918b8c5c2fbbbaff
ayz-lex <andrewyzhanglex@gmail.com>
2020-05-30 13:47:52 -0400

    clean code

--- routes/auth/checkLogin.js
+++ routes/auth/checkLogin.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  res.send(req.session ? 'OK' : 'NO')
+  res.send(req.session.data ? 'OK' : 'NO')
 })
 
 module.exports = router
