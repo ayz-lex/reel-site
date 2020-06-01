b1519cd91cc2c5137c2f7ba6e77e1d9b405d4cf4
ayz-lex <andrewyzhanglex@gmail.com>
2020-06-01 16:26:10 -0400

    structure nav bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import {Navbar, NavDropdown, Nav, Form, FormControl, Button} from 'react-bootstrap'
+import './NavigationBar.css'
 
 class NavigationBar extends React.Component {
   constructor (props) {
@@ -152,44 +152,43 @@ class NavigationBar extends React.Component {
     let tabs
     if (this.state.loggedIn) {
       tabs = (
-        <Navbar className="navigation_bar">
-          <Nav.Link onClick={this.logout()}>logout</Nav.Link>
-        </Navbar>
+        <div>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#logout">Logout</a></li>
+        </div>
       )
     } else {
       tabs = (
-        <Navbar className="navigation_bar" bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
+        <div>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#profile">Profile</a></li>
+          <li class="dropdown">
+            <a href="javascript:void(0)" class="dropbtn">Login</a>
+            <div class="dropdown-content">
+              {this.login()}
+            </div></li>
+          <li class="dropdown">
+            <a href="javascript:void(0)" class="dropbtn">Register</a>
+            <div class="dropdown-content">
+              {this.register()}
+            </div>
+          </li>
+        </div>
       )
     }
     
     return (
-      <Navbar>
-        <Navbar.Brand>Hello</Navbar.Brand>
-        <Navbar.Toggle aria-controls="response-navbar-nav" />
-        <Navbar.Collapse>
+      <div id="navigation_bar">
+        <ul>
           {tabs}
-        </Navbar.Collapse>
-      </Navbar>
+          <li>
+            <form>
+              <input type="text" id="input" name="input"/>
+              <input type="submit"/>
+            </form>
+          </li>
+        </ul>
+      </div>
     )
   }
 }
