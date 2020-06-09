2a5c8fcaeee9f9c9d6a59dba8cce20f7f759bfa8
ayz-lex <andrewyzhanglex@gmail.com>
2020-06-08 20:52:15 -0400

    prepare for streams

--- src/App.js
+++ src/App.js
@@ -6,60 +6,17 @@ import NavigationBar from './components/NavigationBar.js'
 class App extends React.Component{
   constructor(props) {
     super(props)
-    this.state = {movies: [
-      {
-        name: 'name',
-        popularity: 'popularity',
-        genre: 'genre',
-        year: 'year',
-        description: 'description'
-      }, 
-      {
-        name: 'name2',
-        popularity: 'popularity2',
-        genre: 'genre2',
-        year: 'year2',
-        description: 'description2'
-      },
-      {
-        name: 'name3',
-        popularity: 'popularity3',
-        genre: 'genre3',
-        year: 'year3',
-        description: 'description3'
-      }
-    ]}
   }
   
   render() {
     return (
       <div>
         <NavigationBar />
-        <MovieSection movies={this.state.movies}/>
+        <Movie />
       </div>
     )
   }
-
-  /*
-  async componentDidMount() {
-    const response = await fetch('http://localhost:8080/api/movie')
-    const data = await response.json();
-    this.setState({newMovie: data})
-  }
-  */
 }
 
-function MovieSection(props) {
-  return (
-    <div id="movieSection">
-      <h1>Movies</h1>
-      {props.movies.map(movie => (
-        <React.Fragment>
-          <Movie {...movie} />
-        </React.Fragment>
-      ))}
-    </div>
-  )
-}
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,45 +3,90 @@ import './Movie.css'
 import {Dropdown, DropdownButton} from 'react-bootstrap'
 
 class Movie extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {movies: [
+      {
+        name: 'name',
+        popularity: 'popularity',
+        genre: 'genre',
+        year: 'year',
+        description: 'description'
+      }, 
+      {
+        name: 'name2',
+        popularity: 'popularity2',
+        genre: 'genre2',
+        year: 'year2',
+        description: 'description2'
+      },
+      {
+        name: 'name3',
+        popularity: 'popularity3',
+        genre: 'genre3',
+        year: 'year3',
+        description: 'description3'
+      }
+    ]}
+  }
+
+  async getStream() {
+    
+
+  }
+
   render() {
     return (
-      <div id="movie">
-        <div id="watched_button">
-          <DropdownButton
-            drop='right'  
-            title='Watched'          
-          >
-            <Dropdown.Item>Rate the Movie</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item button="5">5</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item button="4">4</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item button="3">3</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item button="2">2</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item button="1">1</Dropdown.Item>
-          </DropdownButton>
-        </div>
-        <div id="title">
-          {this.props.name}
-        </div>
-        <div className="movie_attributes">
-          {this.props.popularity}
-        </div>
-        <div className="movie_attributes">
-          {this.props.genre}
-        </div>
-        <div className="movie_attributes">
-          {this.props.year}
-        </div>
-        <p>
-          {this.props.description}
-        </p>
+      <div id="movieSection">
+        <h1>Movies</h1>
+        {this.state.movies.map(movie => (
+          <React.Fragment>
+            <Individual {...movie} />
+          </React.Fragment>
+        ))}
       </div>
     )
   }
+}
+
+function Individual(props) {
+  return (
+    <div id="movie">
+      <div id="watched_button">
+        <DropdownButton
+          drop='right'  
+          title='Watched'          
+        >
+          <Dropdown.Item>Rate the Movie</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item button="5">5</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item button="4">4</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item button="3">3</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item button="2">2</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item button="1">1</Dropdown.Item>
+        </DropdownButton>
+      </div>
+      <div id="title">
+        {props.name}
+      </div>
+      <div className="movie_attributes">
+        {props.popularity}
+      </div>
+      <div className="movie_attributes">
+        {props.genre}
+      </div>
+      <div className="movie_attributes">
+        {props.year}
+      </div>
+      <p>
+        {props.description}
+      </p>
+    </div>
+  )
 }
 
 export default Movie
