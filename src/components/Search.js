ee96d0757708e1d091494975b1011dfcaac3b200
ayz-lex <andrewyzhanglex@gmail.com>
2020-06-10 16:42:51 -0400

    refine code

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,20 +3,24 @@ import React from 'react'
 class Search extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {movie: {}}
+    this.state = {movie: {}, found: false}
     this.initializer(props.keyword)
   }
 
   initializer = async (keyword) => {
-    let url = `http://localhost:8080/api/movie/${keyword}`
-    let response = await fetch(url)
-    let data = await response.json()
-    this.setState({movie: data})
+    let response = await fetch(`http://localhost:8080/api/search/${keyword}`)
+    if (response.status === 200) {
+      let data = await response.json()
+      this.setState({movie: data, found: true})
+    }
   }
 
   render = () => {
-    return (
+    return found ? (
       <div>hello</div>
+    ) :
+    (
+      <div>not found</div>
     )
   }
   
