ced0a5aa50148250a27dd863731bf4aef296712e
ayz-lex <andrewyzhanglex@gmail.com>
2020-06-18 16:37:07 -0400

    begin query stream

--- api/recommendations.js
+++ api/recommendations.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const fetch = require('node-fetch')
 
-router.get('/', async (req, res) => {
-  let response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=c39010c8ce89b2f71c0f37373a68bfc8').catch(err => {
+router.get('/:page_number', async (req, res) => {
+  let response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=c39010c8ce89b2f71c0f37373a68bfc8&page=${req.params.page_number}`).catch(err => {
     res.status(500).json({
       error: 'Internal Error'
     })
