5f6601570308d54ff6129d472577acd5e893a9f1
ayz-lex <andrewyzhanglex@gmail.com>
2020-07-13 14:08:10 -0400

    incorporate mui components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,21 @@
 import React from 'react'
-import './Movie.css'
-//id serial username charvar 255 password text watched integer[]
+//import './Movie.css'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+})
 
 class Movie extends React.Component {
   constructor(props) {
@@ -42,8 +57,10 @@ class Movie extends React.Component {
   render() {
     return this.state.fetching ? (
       <div> fetching </div>
-    ) : (
+    ) : (      
       this.state.found ? (
+        <MovieCard />
+        /*
         <div> 
           <div id="movie">
             <h1 id="title">
@@ -52,11 +69,36 @@ class Movie extends React.Component {
             <button id="watched_button" onClick={this.watchedHandler}>Watched?</button>
           </div>
         </div>
+        */
       ) : (
       <div> not found</div>
       )
     )
   }
+}
+
+const MovieCard = (props) => {
+  const classes = useStyles()
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color = "textSecondary" gutterBottom>
+          title
+        </Typography>
+        <Typography variant="h5" component="h2">
+          movietitle
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          small text
+        </Typography>
+        <Typography variant="body2" component="p">
+          description
+          <br />
+          post break
+        </Typography>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default Movie
