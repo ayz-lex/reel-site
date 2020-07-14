c8928827bb06dc844495911a6abad0a64f84d4ac
ayz-lex <andrewyzhanglex@gmail.com>
2020-07-14 14:50:39 -0400

    nav bar changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Movie from './components/Movie.js'
 import Watched from './components/Watched.js'
 import Main from './components/Main.js'
+import TextField from '@material-ui/core/TextField'
 import NavigationBar from './components/NavigationBar.js'
 import {BrowserRouter as Router, Switch, Route, useParams} from 'react-router-dom'
 import {LoggedinContext} from './contexts/LoggedinContext'
+import {makeStyles} from '@material-ui/core/styles'
 
 class App extends React.Component{
   constructor(props) {
@@ -76,7 +78,164 @@ class App extends React.Component{
         </div>
       )     
     }
+
+    const useStyles = makeStyles((theme) => ({
+      root: {
+        '& > *': {
+          margin: theme.spacing(1),
+          width: '25ch',
+        },
+      },
+    }))
+
+    const LoginComp = () => {
+      const toggleLoggedIn = this.toggleLoggedIn
+      const toggleLoggedOut = this.toggleLoggedOut
     
+      const [username, setUsername] = useState('')
+      const [password, setPassword] = useState('')
+    
+      const usernameChangeHandler = e => {
+        setUsername(e.target.value)
+      }
+      const passwordChangeHandler = e => {
+        setPassword(e.target.value)
+      }
+    
+      const loginSubmitHandler = async e => {
+        e.preventDefault()
+    
+        await fetch('http://localhost:8080/api/login', {
+          method: 'POST',
+          withCredentials: 'true',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify({
+            username: username, 
+            password: password
+          })
+        }).then(res => {
+          if (res.status === 200) {
+            alert('Login Succeeded')
+            toggleLoggedIn()
+          } else {
+            const error = new Error(res.error)
+            throw error
+          }
+        }).catch(err => {
+          console.error(err)
+          alert('Error logging in')
+          toggleLoggedOut()
+        })
+      }
+
+      const classes = useStyles()
+    
+      return (  
+        <div>
+          <form className={classes.root} onSubmit = {loginSubmitHandler}>
+            <div>
+              <TextField
+                type = 'text' 
+                name = 'username' 
+                variant='outlined'
+                label='Enter Username'
+                onChange = {usernameChangeHandler}
+                required
+              />
+            </div>
+            <div>
+              <TextField 
+                type = 'text' 
+                name = 'password'
+                variant='outlined'
+                label='Enter Password'
+                onChange = {passwordChangeHandler}
+                required
+              />
+            </div>
+            <button>Login</button>
+          </form>
+        </div>
+      )
+    }
+
+    const SignupComp = () => {
+      const toggleLoggedIn = this.toggleLoggedIn
+      const toggleLoggedOut = this.toggleLoggedOut
+
+      const [username, setUsername] = useState('')
+      const [password, setPassword] = useState('')
+
+      const usernameChangeHandler = e => {
+        setUsername(e.target.value)
+      }
+      const passwordChangeHandler = e => {
+        setPassword(e.target.value)
+      }
+
+      const registerSubmitHandler = async e => {
+        e.preventDefault()
+        await fetch('http://localhost:8080/api/register', {
+          method: 'POST',
+          withCredentials: 'true',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify({
+            username: username, 
+            password: password
+          })
+        }).then(res => {
+          if (res.status === 200) {
+            alert('Register Succeeded')
+            toggleLoggedIn()
+          } else {
+            const error = new Error(res.error)
+            throw error
+          }
+        }).catch(err => {
+          alert('Register Failed')
+          toggleLoggedOut()
+        })
+      }
+
+      const classes = useStyles()
+
+      return (
+        <div>
+          <form className={classes.root} onSubmit = {registerSubmitHandler}>
+            <div>
+              <TextField
+                type='text'
+                name="username_register"
+                label="Enter Username"
+                variant="outlined"
+                onChange={usernameChangeHandler}
+                required
+              />
+            </div>
+            <div>
+              <TextField 
+                type='text' 
+                name='password_register'
+                label="Enter Password"
+                variant="outlined"
+                onChange={passwordChangeHandler}
+                required
+              />
+            </div>
+            <button>Register</button>
+          </form>
+        </div>
+      )
+    }
+     
     return (
       <div>
         <LoggedinContext.Provider value={this.state}>
@@ -86,6 +245,8 @@ class App extends React.Component{
               <Switch>
                 <Route path="/movie/:movie_id" component={MovieComp}/>
                 <Route path="/profile" component={ProfileComp} />
+                <Route path="/login" component={LoginComp} />
+                <Route path="/signup" component={SignupComp} />
                 <Route exact path="/" component={MainComp}/>
               </Switch>
             </div>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -12,35 +12,6 @@ class NavigationBar extends React.Component {
       <div id="navigation_bar">
         <LoggedinContext.Consumer>
           {({isLoggedIn, toggleLoggedIn, toggleLoggedOut}) => (
-            /*
-            <ul>
-              {isLoggedIn ? (
-                <div>
-                <li><a href="/">Home</a></li>
-                <li><a href="/profile">Profile</a></li>
-                <li>
-                  <LogoutButton toggleLoggedOut={toggleLoggedOut}/>
-                </li>
-              </div>
-              ) : (
-                <div>
-                  <li><a href="/">Home</a></li>
-                  <li class="dropdown">
-                    <a href="javascript:void(0)" class="dropbtn">Login</a>
-                    <div class="dropdown-content">
-                      <LoginButton toggleLoggedIn={toggleLoggedIn} toggleLoggedOut={toggleLoggedOut} />
-                    </div>
-                  </li>
-                  <li class="dropdown">
-                    <a href="javascript:void(0)" class="dropbtn">Register</a>
-                    <div class="dropdown-content">
-                      <RegisterButton toggleLoggedIn={toggleLoggedIn} toggleLoggedOut={toggleLoggedOut} />
-                    </div>
-                  </li>
-                </div>
-              )}
-            </ul>
-            */
            <NavBar isLoggedIn={isLoggedIn} toggleLoggedIn={toggleLoggedIn} toggleLoggedOut={toggleLoggedOut} />
           )}
         </LoggedinContext.Consumer>
@@ -67,28 +38,39 @@ const StyledBreadcrumb = withStyles((theme) => ({
 
 const NavBar = (props) => {
   return (
-    <Breadcrumbs aria-label="breadcrumb">
-      <StyledBreadcrumb 
+    props.isLoggedIn ? (
+      <Breadcrumbs aria-label="breadcrumb">
+        <StyledBreadcrumb 
           component="a"
           href="/"
           label="Home"
-      />
-      {props.isLoggedIn ? (
-        <React.Fragment>
-          <StyledBreadcrumb 
-            component="a"
-            href="/profile"
-            label="Profile"
-          />
-          <LogoutButton toggledLoggedOut={props.toggleLoggedOut} />
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <LoginButton toggleLoggedIn={props.toggleLoggedIn} toggleLoggedOut={props.toggleLoggedOut} />
-          <RegisterButton toggleLoggedIn={props.toggleLoggedIn} toggleLoggedOut={props.toggleLoggedOut} />
-        </React.Fragment>
-      )}
-    </Breadcrumbs>
+        />
+        <StyledBreadcrumb 
+          component="a"
+          href="/profile"
+          label="Profile"
+        />
+        <LogoutButton toggledLoggedOut={props.toggleLoggedOut} />
+      </Breadcrumbs>
+    ) : (
+      <Breadcrumbs aria-label="breadcrumb">
+        <StyledBreadcrumb 
+          component="a"
+          href="/"
+          label="Home"
+        />
+        <StyledBreadcrumb 
+          component="a"
+          href="/login"
+          label="Login"
+        />
+        <StyledBreadcrumb 
+          component="a"
+          href="/signup"
+          label="Signup"
+        />
+      </Breadcrumbs>
+    )
   )
 }
 
@@ -117,141 +99,4 @@ const LogoutButton = (props) => {
     />
   )
 }
-
-const LoginButton = (props) => {
-  const toggleLoggedIn = props.toggleLoggedIn
-  const toggleLoggedOut = props.toggleLoggedOut
-
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const usernameChangeHandler = e => {
-    setUsername(e.target.value)
-  }
-  const passwordChangeHandler = e => {
-    setPassword(e.target.value)
-  }
-
-  const loginSubmitHandler = async e => {
-    e.preventDefault()
-
-    await fetch('http://localhost:8080/api/login', {
-      method: 'POST',
-      withCredentials: 'true',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        username: username, 
-        password: password
-      })
-    }).then(res => {
-      if (res.status === 200) {
-        alert('Login Succeeded')
-        toggleLoggedIn()
-      } else {
-        const error = new Error(res.error)
-        throw error
-        toggleLoggedOut()
-      }
-    }).catch(err => {
-      console.error(err)
-      alert('Error logging in')
-      toggleLoggedOut()
-    })
-  }
-
-  return (  
-    <div>
-      <form onSubmit = {loginSubmitHandler}>
-        <div>
-          <input
-            type = 'text' 
-            name = 'username' 
-            onChange = {usernameChangeHandler}
-          />
-        </div>
-        <div>
-          <input 
-            type = 'text' 
-            name = 'password'
-            onChange = {passwordChangeHandler}
-          />
-        </div>
-        <button>Login</button>
-      </form>
-    </div>
-  )
-}
-
-const RegisterButton = (props) => {
-  const toggleLoggedIn = props.toggleLoggedIn
-  const toggleLoggedOut = props.toggleLoggedOut
-
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const usernameChangeHandler = e => {
-    setUsername(e.target.value)
-  }
-  const passwordChangeHandler = e => {
-    setPassword(e.target.value)
-  }
-
-  const registerSubmitHandler = async e => {
-    e.preventDefault()
-    await fetch('http://localhost:8080/api/register', {
-      method: 'POST',
-      withCredentials: 'true',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        username: username, 
-        password: password
-      })
-    }).then(res => {
-      if (res.status === 200) {
-        alert('Register Succeeded')
-        toggleLoggedIn()
-      } else {
-        const error = new Error(res.error)
-        throw error
-        toggleLoggedOut()
-      }
-    }).catch(err => {
-      alert('Register Failed')
-      toggleLoggedOut()
-    })
-  }
-
-  return (
-    <div>
-      <form onSubmit = {registerSubmitHandler}>
-        <div>
-          <input
-            type = 'text' 
-            name = 'username_register' 
-            onChange = {usernameChangeHandler}
-            required
-          />
-        </div>
-        <div>
-          <input 
-            type = 'text' 
-            name = 'password_register'
-            onChange = {passwordChangeHandler}
-            required
-          />
-        </div>
-        <button>Register</button>
-      </form>
-    </div>
-  )
-}
-
 export default NavigationBar
