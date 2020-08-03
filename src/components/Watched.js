44986539b26c861724abb3c68ebe77dd4784443b
ayz-lex <andrewyzhanglex@gmail.com>
2020-08-03 16:17:31 -0400

    add interactive styling to watched page

--- src/components/Watched.js
+++ src/components/Watched.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {
   GridList,
-  GridListTile
+  GridListTile,
+  AppBar,
+  Toolbar,
+  Typography
 } from "@material-ui/core";
 
-import {makeStyles} from '@material-ui/core/styles'
-
-
+import {makeStyles, withStyles} from '@material-ui/core/styles'
 
 function Watched() {
 
@@ -14,31 +15,37 @@ function Watched() {
   const [fetching, setFetching] = useState(true)
 
   const fetchSearch = async () => {
-    const response = await fetch('http://localhost:8080/api/watched', {
-      method: 'GET',
-      withCredentials: 'true',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-    })
-    const data = await response.json()
+    if (fetching) {
+      const response = await fetch('http://localhost:8080/api/watched', {
+        method: 'GET',
+        withCredentials: 'true',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+      })
+      const data = await response.json()
 
-    const movieList = await Promise.all(data.map(async movie => {
-      const response = await fetch(`http://localhost:8080/api/movie/${movie}`)
+      const movieList = await Promise.all(data.map(async movie => {
+        const response = await fetch(`http://localhost:8080/api/movie/${movie}`)
 
-      const movieData = await response.json()
+        const movieData = await response.json()
 
-      return {
-        img: `https://image.tmdb.org/t/p/w400/${movieData.poster_path}`,
-        title: movieData.title
-      }
-    }))
+        return {
+          img: `https://image.tmdb.org/t/p/w400/${movieData.poster_path}`,
+          title: movieData.title
+        }
+      }))
 
-    setMovieArray(movieList)
-    setFetching(false)
+      setMovieArray(movieList)
+      setFetching(false)
+    }
   }
+
+  useEffect(() => {
+    fetchSearch()
+  })
 
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -46,29 +53,80 @@ function Watched() {
       flexWrap: 'wrap',
       justifyContent: 'space-around',
       overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      maxWidth: 500,
+      maxHeight: 600,
+      borderColor: theme.palette.grey[800],
+    },
+    appBar: {
+      marginBottom: 2,
+      backgroundColor: theme.palette.grey[100],
+      maxWidth: 500,
+      boxShadow: 'none',
+    },
+    title: {
+      color: theme.palette.grey[800],
+      flexGrow: 1,
     },
     gridList: {
-      width: 500,
-      height: 450,
+      display: 'flex',
+      flexDirection: 'row',
+      maxWidth: 500,
+      maxHeight: 500,
+      backgroundColor: theme.palette.background.paper,
     },
+    tile: {
+      '&:hover img': {
+        opacity: 0.3
+      },
+      '&:hover h1': {
+        opacity: 1
+      }
+    },
+    imgTitle: {
+      transition: '.5s ease',
+      opacity: 0,
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      textAlign: 'center',
+      fontSize: '16px',
+      color: 'black',
+      fontWeight: 'bold'
+    },
+    img: {
+      opacity: 1,
+      display: 'block',
+      transition: '.5s ease',
+      backfaceVisibility: 'hidden',
+    }
   }));
 
   const classes = useStyles()
-    
+
   return (
     fetching ? (
-      <React.Fragment>{fetchSearch()}</React.Fragment>
+      <React.Fragment></React.Fragment>
     ) : (
-      <div>
-        <GridList cellHeight={160} className={classes.gridList} cols={3}>
-          {movieArray.map((tile) => (
-            <GridListTile key={tile.img} cols={tile.cols || 1}>
-              <img src={tile.img} alt={tile.title} />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
+        <div className={classes.root}>
+          <AppBar position="static" color="primary" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                Watched   |   {movieArray.length} Movies
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <GridList cellHeight={160} className={classes.gridList} cols={3}>
+            {movieArray.map((tile) => (
+              <GridListTile className={classes.tile} key={tile.img} cols={tile.cols || 1}>
+                <img className={classes.img} src={tile.img} alt={tile.title} />
+                <Typography variant='h1' className={classes.imgTitle}>{tile.title}</Typography>
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
     )
   )
 }
